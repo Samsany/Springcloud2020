b7224062175235591c1d4e1cbd82c2ee91fa653f
Samsany <1451691457@qq.com>
2020-04-25 14:29:33 +0800

    SpringCloud Config分布式配置中心

---
+++ cloud-config-center-3355/src/main/java/com/samphanie/springcloud/ConfigCenterMain3355.java
@@ -0,0 +1,19 @@
+package com.samphanie.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname ConfigCenterMain3355
+ * @Date 2020/4/25 11:39
+ * @Author ZSY
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigCenterMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3355.class,args);
+    }
+}
